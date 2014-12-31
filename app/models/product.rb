92c43e5415b04f8d078b5a8fba968bb8980b4334
gabriel <gabriel.c1901@gmail.com>
2014-12-31 12:07:10 -0300

    added store's catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   # validates :image_url, allow_blank: true
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)\z}i
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -37,13 +37,13 @@ class ProductTest < ActiveSupport::TestCase
   test "image url" do
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
       http://a.b.c/x/y/z/fred.gif }
-      bad = %w{ fred.doc fred.gif/more fred.gif.more }
-      ok.each do |name|
-        assert new_product(name).valid?, "#{name} should be valid"
-      end
-      bad.each do |name|
-        assert new_product(name).invalid?, "#{name} shouldn't be valid"
-      end
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
   end
 
   test "product is not valid without a unique title" do
